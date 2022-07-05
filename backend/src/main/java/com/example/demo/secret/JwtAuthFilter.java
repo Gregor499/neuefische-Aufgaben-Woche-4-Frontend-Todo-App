8f6f1311e4869cf6957ea401d6ce9854dcec00f3
Gregor499 <postfach@gregor-dirks.de>
2022-07-05 13:03:27 +0200

    added missing depedencies, jwtService and SecurityConfig classes began adding SecurityConfig next step will be adding all user classes

---
+++ backend/src/main/java/com/example/demo/secret/JwtAuthFilter.java
@@ -0,0 +1,27 @@
+package com.example.demo.secret;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+private final JwtService jwtService;
+private final UserService userService;
+
+    public JwtAuthFilter(JwtService jwtService, UserService userService) {
+        this.jwtService = jwtService;
+        this.userService = userService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+    }
+}
