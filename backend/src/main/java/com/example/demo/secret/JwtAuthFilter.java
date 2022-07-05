55a38fc89676ab7cf582b6354ba4613350fc7def
Gregor499 <postfach@gregor-dirks.de>
2022-07-05 19:43:35 +0200

    completed JwtAuthFilter

--- backend/src/main/java/com/example/demo/secret/JwtAuthFilter.java
+++ backend/src/main/java/com/example/demo/secret/JwtAuthFilter.java
@@ -1,5 +1,10 @@
 package com.example.demo.secret;
 
+import io.jsonwebtoken.Claims;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -8,12 +13,13 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @Component
 public class JwtAuthFilter extends OncePerRequestFilter {
 
-private final JwtService jwtService;
-private final UserService userService;
+    private final JwtService jwtService;
+    private final UserService userService;
 
     public JwtAuthFilter(JwtService jwtService, UserService userService) {
         this.jwtService = jwtService;
@@ -22,6 +28,33 @@ private final UserService userService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = getToken(request);
+        if (token != null && !token.isBlank()) {
+            try {
+                Claims claims = jwtService.extractAllClaims(token);
+                setSecurityContext(claims);
+            } catch (Exception e) {
+                response.setStatus(401);
+            }
+        }
 
+        filterChain.doFilter(request, response);
+    }
+
+    private String getToken(HttpServletRequest request) {
+        String authorization = request.getHeader("Authorization");
+        if (authorization != null) {
+            return authorization.replace("Bearer", "").trim();
+        }
+        return null;
+    }
+
+    private void setSecurityContext(Claims claims) {
+        List<SimpleGrantedAuthority> grantedAuthorities = claims.get("roles") == null ? List.of() : ((List<String>) claims.get("roles")).stream().map(SimpleGrantedAuthority::new).toList();
+
+        User user = userService.findById(claims.getSubject()).orElseThrow();
+
+        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(user.getUsername(), "", grantedAuthorities);
+        SecurityContextHolder.getContext().setAuthentication(token);
     }
 }
